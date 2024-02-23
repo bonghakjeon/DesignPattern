0c013e62b3208087d0604a678a8822f62f5d7bdb
DESKTOP-GUHF3TT\bhjeon <bhjeon@imbu.co.kr>
2024-02-23 15:59:24 +0900

    디자인 패턴 ProtoType 커밋

---
+++ ProtoType/ProtoType.java
@@ -0,0 +1,5 @@
+package DesignPattern.ProtoType;
+
+public interface ProtoType {
+    Object copy(); // 새로운 객체를 복사해서 생성
+}
---
+++ ProtoType/Shape.java
@@ -0,0 +1,9 @@
+package DesignPattern.ProtoType;
+
+public interface Shape {
+    String draw();
+
+    void moveOffset(int dx, int dy);
+}
+
+
